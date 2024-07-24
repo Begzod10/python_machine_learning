366aa859288088d59dcca9b6fa8a319b58449a6f
Begzod10 <rimefara22@gmail.com>
2024-07-24 10:41:05 -0800

    commit

--- new_pandas/8-metodi pandas dlya texta.py
+++ new_pandas/8-metodi pandas dlya texta.py
@@ -1,6 +1,17 @@
-list_questions = [
-    {
-        "number": 1,
-        "value": "Agar qiyin ishlarni bajarib charchasam, darhol uni tashlab qo'yaman"
-    }
-]
+import numpy as np
+import pandas as pd
+
+email = 'rimefara@gamil.com'
+
+names = pd.Series(['andrew', 'bobo', 'claire', 'david', '5'])
+# names.str -> string metodlarini ishlatsa boladi
+text_finance = ['GOOG,APPL,AMZN', 'JPN,BAC,GS']
+tickers = pd.Series(text_finance)
+tech = 'GOOG,APPL,AMZN'
+messy_names = pd.Series(["andrew  ","bo;bo","  claire  "])
+
+def cleanup(name):
+    name = name.replace(";", "")
+    name = name.strip()
+    name = name.capitalize()
+    return name
